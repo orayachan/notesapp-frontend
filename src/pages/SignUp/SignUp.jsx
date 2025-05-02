d8755e76f3f69a7d425dba6b05619daa621938f1
Oraya Chantanawat <ochantanawat@gmail.com>
2025-05-02 14:39:41 +0700

    fix(style): adjust border radius of Login and SignUp containers

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -40,7 +40,7 @@ const SignUp = () => {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-fuchsia-50 px-4">
-      <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
+      <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-md">
         <h2 className="text-2xl font-bold text-fuchsia-900 mb-6 text-center">
           Sign Up for NotesApp
         </h2>
